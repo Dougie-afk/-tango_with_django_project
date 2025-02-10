353e64c86144bcd5d3b6f6219681fb26631f1757
Douglas Lande <2891090L@student.gla.ac.uk>
2025-02-10 10:20:28 +0000

    Added index template and directory

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tango_with_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:\Users\Douglas Lande\Computing\Workspace\tango_with_django_project\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
