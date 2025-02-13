3a3a648d3d9b5762ba14707d5b6b08b6f7353d1f
Douglas Lande <2891090L@student.gla.ac.uk>
2025-02-13 16:25:01 +0000

    Views added

--- populate_rango.py
+++ populate_rango.py
@@ -10,11 +10,14 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url':'http://docs.python.org/3/tutorial/'},
+        'url':'http://docs.python.org/3/tutorial/',
+        'views':103},
         {'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'},
+        'url':'http://www.greenteapress.com/thinkpython/',
+        'views':67},
         {'title':'Learn Python in 10 Minutes',
-        'url':'http://www.korokithakis.net/tutorials/python/'} ]
+        'url':'http://www.korokithakis.net/tutorials/python/',
+        'views':25} ]
 
     django_pages = [
         {'title':'Official Django Tutorial',
